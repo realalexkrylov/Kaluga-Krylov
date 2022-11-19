9dbf2119c43b76027e0fc51f3c2ae5478edd3f8f
alex40klg <alex40klg@gmail.com>
2022-11-19 17:39:49 +0300

    ended

--- oct22.py
+++
@@ -1,9 +0,0 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('And I change it every day')
-    print('Hello 2')
-
-
-if __name__ == '__main__':
-    main()
